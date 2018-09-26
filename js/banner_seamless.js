d88e42e8f226d553b409fd4678234464624cd46a
heshijia <42805166+heshij@users.noreply.github.com>
2018-09-26 19:14:02 +0800

    Update banner_seamless.js
    改为面向对象写法

--- js/banner_seamless.js
+++ js/banner_seamless.js
@@ -1,3 +1,5 @@
+/*
+//面向过程的写法
 $(function () {
 
     var $tabLi = $('#banner .tab li');
@@ -81,5 +83,112 @@ $(function () {
             }
         });
     }
+
+});
+*/
+
+//面向对象写法
+$(function () {
+
+    function Banner(ele) {
+        this.$tabLi = $(ele.tabLi);//小圆点
+        this.$picUl = $(ele.picUl);
+        this.$banner = $(ele.banner);
+        this.$btn = $(ele.btn);
+        this.$btnA = $(ele.btnA);
+        this.imgWidth = $(ele.picLi).width();
+        this.index = 0;
+        this.nowTime = new Date();
+        this.timer = null;
+        this.tabLiClick();//小圆点点击切换banner
+        this.bannerHover();//鼠标移入停止banner
+        this.btnAClick();//左右按钮点击切换banner
+        this.autoPlay();//自动切换banner
+    }
+
+    Banner.prototype.tabLiClick = function () {
+        var _this = this;
+        _this.$tabLi.click(function () {
+            //获取当前的序列号
+            _this.index = $(this).index();
+            $(this).addClass('on').siblings().removeClass('on');
+            //动画animate({属性 ： 值}，时间);
+            _this.$picUl.animate({
+                marginLeft: -_this.imgWidth * (_this.index + 1) + 'px'
+            }, 300);
+        });
+    };
+
+    Banner.prototype.bannerHover = function () {
+        var _this = this;
+        _this.$banner.hover(function () {
+            _this.$btn.show();
+            clearInterval(_this.timer);
+        }, function () {
+            _this.$btn.hide();
+            _this.timer = setInterval(function () {
+                _this.index++;
+                fn(_this);
+            }, 2000);
+        });
+    };
+
+    Banner.prototype.btnAClick = function () {
+        var _this = this;
+        _this.$btnA.click(function () {
+            if (new Date() - _this.nowTime > 300)//当前时间减去上一次时间大于300毫秒才执行
+            {
+                _this.nowTime = new Date();
+                var i = $(this).index();
+                //判断点击哪个按钮
+                i ? _this.index++ : _this.index--;
+                fn(_this);
+            }
+        }).mousedown(function () {
+            return false;
+        });
+    };
+
+    Banner.prototype.autoPlay = function () {
+        var _this = this;
+        //开启定时器
+        _this.timer = setInterval(function () {
+            _this.index++;
+            fn(_this);
+        }, 2000);
+    };
+
+    function fn(_this) {
+        var liIndex = _this.index;
+        //判断下标，最后一张时按钮也要回到第一个
+        if (liIndex >= _this.$tabLi.length) {
+            liIndex = 0;
+        }
+        else if (liIndex < 0) {
+            liIndex = _this.$tabLi.length - 1;
+        }
+        _this.$tabLi.eq(liIndex).addClass('on').siblings().removeClass('on');
+        _this.$picUl.animate({
+            marginLeft: -_this.imgWidth * (_this.index + 1) + 'px'
+        }, 300, function () {//回调函数判断，让图片瞬间复位
+            if (_this.index == _this.$tabLi.length) {
+                _this.$picUl.css('marginLeft', -_this.imgWidth + 'px');
+                _this.index = 0;
+            }
+            else if (_this.index < 0) {
+                _this.$picUl.css('marginLeft', -_this.imgWidth * (_this.$tabLi.length) + 'px');
+                _this.index = _this.$tabLi.length - 1;
+            }
+        });
+    }
+
+    var banner = new Banner({
+        tabLi: '#banner .tab li',
+        picUl: '#banner .pic ul',
+        banner: '#banner',
+        btn: '#banner .btn',
+        btnA: '#banner .btn a',
+        picLi: '#banner .pic li'
+    });
 
 });
